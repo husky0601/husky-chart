3ce7ff32d71ef72df1e4ebca17621749da9f2b9a
husky <suyating228@gmail.com>
2020-02-16 23:34:48 +0800

    init husky-chart

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+import svgr from '@svgr/rollup'
+import json from 'rollup-plugin-json'
+
+import pkg from './package.json'
+
+export default {
+    input: 'src/index.js',
+    output:[
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        }
+    ],
+    external: ["react", "lodash"],
+    experimentalCodeSplitting: true,
+    plugins:[
+        json({
+            exclude: 'node_modules/**'
+        }), 
+        external(),
+        postcss({
+          modules: true
+        }),
+        url(),
+        svgr(),
+        babel({
+            exclude: ['node_modules/**', '*.json'],
+        }),
+        resolve(),
+        commonjs({
+            include: ["node_modules/**"]
+        })
+    ]
+}
---
+++ src/Chart/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Chart extends React.Component{
+    render(){
+        return <div>
+            Hello Test Chart
+        </div>
+    }
+}
+export default Chart
