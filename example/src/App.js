977615d773944e620efd9d9a19d5be7308f11bd6
husky0601 <suyating228@gmail.com>
2020-02-23 15:08:23 +0800

    add example

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chart from 'husky-chart'
+
+function App () {
+    return <div>
+        App
+        <Chart />
+    </div>
+}
+export default App
--- src/Chart/index.js
+++ src/Chart/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
-class Chart extends React.Component{
-    render(){
-        return <div>
-            Hello Test Chart
-        </div>
-    }
+function Chart(){
+    return <div>Hello Test Chart</div>
 }
 export default Chart
